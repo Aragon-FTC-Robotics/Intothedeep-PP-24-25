0b95a114c631059f5ced4b2acfd2551a5019486d
twenty-one21 <31107399+twenty-one21@users.noreply.github.com>
2024-12-29 13:50:57 -0800

    Update Auto_0_4.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_0_4.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_0_4.java
@@ -2,13 +2,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.localization.*;
-import org.firstinspires.ftc.teamcode.pedroPathing.follower.*;
-import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.*;
-import org.firstinspires.ftc.teamcode.pedroPathing.util.*;
-import org.firstinspires.ftc.teamcode.mechanisms.*;
 
-@Autonomous(name = "0+3", group = "Auto")
-public class Auto_0_3 {
+@Autonomous(name = "0+4", group = "Auto")
+public class Auto_0_4 {
     private final Pose STARTPOSE = new Pose(); //3 arguments: x, y, heading (in radians)
     private final Pose PRELOADPOSE = new Pose();
     private final Pose PRELOADCONTROL = new Pose();
@@ -18,4 +14,6 @@ public class Auto_0_3 {
     private final Pose BUCKETPOSE = new Pose();
     private final Pose ASCENTPOSE = new Pose();
     private final Pose ASCENTCONTROL1 = new Pose();
+
+    
 }
