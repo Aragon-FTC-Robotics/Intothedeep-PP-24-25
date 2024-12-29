99cdaa50623ed6fa18a9213cd74395afeac4d716
ABlueberryC <clairedong9@gmail.com>
2024-12-29 13:50:49 -0800

    testop fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testmechanisms/ClawTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testmechanisms/ClawTest.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class ClawTest {
     Servo claw;
-    public static double POS = 0;
+    public static double POS = 0.737;
     public void init(HardwareMap hm) {
         claw = hm.get(Servo.class, "intakeWrist");
     }
