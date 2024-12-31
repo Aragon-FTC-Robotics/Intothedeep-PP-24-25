bacdd1e36940a179a63a376fcf2942f8cd17e0e4
twenty-one21 <31107399+twenty-one21@users.noreply.github.com>
2024-12-31 14:11:53 -0800

    more bugfixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testmechanisms/WristTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testmechanisms/WristTest.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 @Config
 public class WristTest {
     Servo wrist;
-    public static double POS = 1;
+    public static double POS = 0.000;
 
     public void init(HardwareMap hm) {
         wrist = hm.get(Servo.class, "wrist");
